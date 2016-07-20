4d896ddb24f9f6cc519bbe99b7533eb60ac28aac
jhst333 <jhst333@gmail.com>
2016-07-20 14:40:48 +0200

    example

---
+++ examples/get_info.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require 'minimal-facebook-api'
+require 'colorize'
+
+def display_info
+ puts "[*] #{__FILE__.colorize(:color => :red)} <login> <password> <uid | link>"
+ puts "[*] This script displays info about certain facebook profile"
+ exit
+end
+
+def main
+ display_info if ARGV.size != 3
+ puts "[*] Attempting to login"
+ begin
+  interface = Facebook::Authorize.new(:login => ARGV[0], :password => ARGV[1]).interface
+  puts "[*] Searching for #{ARGV[2].colorize(:color => :cyan)}"
+  if ARGV[2] =~ /^\d+$/
+   account = interface.account(:uid => ARGV[2])
+  else
+   account = interface.account(:link => ARGV[2])
+  end
+  account.info.each_pair do |key, value|
+   puts "[+] #{key.to_s.colorize(:color => :green)} => #{value.colorize(:color => :yellow)}"
+  end
+ rescue RuntimeError => error
+  puts "[!] ".colorize(:color => :red)
+  puts error.to_s
+ end
+end
+
+main
